1998cc769b0188044a98e4b137cdfbeaaf96fef8
dbornilla <78960805+dbornilla@users.noreply.github.com>
2021-11-09 23:33:00 +0800

    Final

--- main.py
+++ main.py
@@ -250,7 +250,6 @@ class Navigation:
 
             completed_projects.close()
 
-            print('Press any key to continue.')
 
         except FileNotFoundError:
             print('File path not found')
@@ -280,8 +279,6 @@ class Navigation:
 
             all_projects.close()
 
-            print('Press any key to continue.')
-
         except FileNotFoundError:
             print('File path not found')
 
@@ -290,12 +287,15 @@ class Navigation:
         """
         This method will print a text base table format to display projects.
         """
+
         print("ID Number : Title       : Size : Priority ")
+
         for i in list_project:
             print(i[0], " " * (8 - len(i[0])), ":",
                   i[1], " " * (10 - len(i[1])), ":",
                   i[2], " " * (3 - len(i[2])), ":",
                   i[3], end="")
+        print("")
 
     def schedule_projects_submenu(self):
         """
